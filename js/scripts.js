55647d4668513ab95841132c0e699df53ed31525
Genevieve Latimer and Niki Garner <nbarfield5@gmail.com>
2016-03-16 20:49:35 -0700

    Attempt to organize to do list by day and nest tasks inside.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,23 @@
 //business logic
-function List(chores, appointments, dinner) {
+//object that stores daily tasks
+function dailyTask(day, chores, appointments, dinner) {
+  this.day = day-of-the-week;
   this.chores = chores;
   this.appointments = appointments;
   this.dinner = dinner;
 }
 
+
 //user logic
+//store user input value
+
 $(document).ready(function() {
   $("form#items").sumbit(function(event){
     event.preventDefault();
+    var newItem = $("input#new-item").val();
+
+
+    $("itemized-list").append("<li><span class='newItem'" + newItem.__() "</span></li")
 
   });
 });
